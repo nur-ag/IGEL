59f540f10c234bc3f1471ea05bd20136638912f7
Nur Álvarez González <nurloco@hotmail.com>
2022-01-03 20:35:35 +0100

    Modify IGEL embedding methods for reproducing GNN matlang results

--- gnnml-comparisons/igel_embedder.py
+++ gnnml-comparisons/igel_embedder.py
@@ -8,24 +8,31 @@ from learning import set_seed
 
 
 TRAINING_OPTIONS = TrainingParameters(batch_size=10000, learning_rate=0.1, weight_decay=0.0, epochs=2, display_epochs=1, batch_samples_fn='uniform', problem_type='unsupervised')
+NEGATIVE_SAMPLING_OPTIONS = NegativeSamplingParameters(random_walk_length=15, window_size=3, negatives_per_positive=10, minimum_negative_distance=1)
 
+DEBUG = False
+if DEBUG:
+    TRAINING_OPTIONS = TrainingParameters(batch_size=10000, learning_rate=0.1, weight_decay=0.0, epochs=1, display_epochs=1, batch_samples_fn='uniform', problem_type='unsupervised')
+    NEGATIVE_SAMPLING_OPTIONS = NegativeSamplingParameters(random_walk_length=1, window_size=1, negatives_per_positive=1, minimum_negative_distance=1)
 
 def get_unsupervised_embeddings(G, 
                                 distance, 
                                 seed=0,
                                 vector_length=5,
                                 train_opts=TRAINING_OPTIONS):
-    set_seed(seed)
-    nsp = NegativeSamplingParameters(
-        random_walk_length=15,
-        window_size=3,
-        negatives_per_positive=10,
-        minimum_negative_distance=1
-    )
+    return get_unsupervised_embedder(G, distance, seed, vector_length, train_opts)(G.vs, G).cpu().detach().numpy()
 
+
+def get_unsupervised_embedder(G, 
+                              distance, 
+                              seed=0,
+                              vector_length=5,
+                              train_opts=TRAINING_OPTIONS):
+    set_seed(seed)
+    nsp = NEGATIVE_SAMPLING_OPTIONS
     device = T.device('cuda') if T.cuda.is_available() else T.device('cpu')
 
     mapper = StructuralMapper(G, distance, use_distances=True, num_workers=1, device=device)
     model = SimpleStructuralEmbedder(vector_length, mapper, counts_transform='log', device=device)
     trained_model = train_negative_sampling(G, model, nsp, train_opts, device)
-    return trained_model(G.vs, G).cpu().detach().numpy()
+    return trained_model
