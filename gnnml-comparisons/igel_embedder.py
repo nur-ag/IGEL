22f56cc4bc71b0d207cec10e6dfe844dfa8c69d9
Nur Álvarez González <nurloco@hotmail.com>
2022-11-19 16:05:43 +0100

    Introduce relative degrees in the utils embedder, add support for non-seeded runs to be used with GNN-AK.

--- gnnml-comparisons/igel_embedder.py
+++ gnnml-comparisons/igel_embedder.py
@@ -19,20 +19,23 @@ def get_unsupervised_embeddings(G,
                                 distance, 
                                 seed=0,
                                 vector_length=5,
+                                use_relative_degrees=False,
                                 train_opts=TRAINING_OPTIONS):
-    return get_unsupervised_embedder(G, distance, seed, vector_length, train_opts)(G.vs, G).cpu().detach().numpy()
+    return get_unsupervised_embedder(G, distance, seed, vector_length, use_relative_degrees, train_opts)(G.vs, G).cpu().detach().numpy()
 
 
 def get_unsupervised_embedder(G, 
                               distance, 
                               seed=0,
                               vector_length=5,
+                              use_relative_degrees=False,
                               train_opts=TRAINING_OPTIONS):
-    set_seed(seed)
+    if seed is not None:
+        set_seed(seed)
     nsp = NEGATIVE_SAMPLING_OPTIONS
     device = T.device('cuda') if T.cuda.is_available() else T.device('cpu')
 
-    mapper = StructuralMapper(G, distance, use_distances=True, num_workers=1, device=device)
+    mapper = StructuralMapper(G, distance, use_distances=True, num_workers=1, device=device, use_relative_degrees=use_relative_degrees)
     model = SimpleStructuralEmbedder(vector_length, mapper, counts_transform='log', device=device)
     trained_model = train_negative_sampling(G, model, nsp, train_opts, device)
     return trained_model
--- src/structural.py
+++ src/structural.py
@@ -7,21 +7,47 @@ from multiprocessing import cpu_count, Pool
 from collections import Counter
 
 
+def get_relative_degrees(node, dist_vector):
+    '''Computes the relative degree of a node given a distance vector indexed by node indices.
+
+    Relative degrees are the counts of edges from one node to neighbours that are:
+
+    -1: one step closer,
+     0: at the same distance, or
+     1: one step further away
+
+    from a third node towards which relative positioning is computed.
+    '''
+    outgoing_edge_indices = [i for e in node.all_edges() for i in e.tuple if i != node.index]
+    distances = [dist_vector[index] - dist_vector[node.index] for index in outgoing_edge_indices]
+    distance_freqs = {d: c for d, c in Counter(distances).most_common()}
+    return [distance_freqs.get(i, 0) for i in [-1, 1]] + [node.degree()]
+
+
 def compute_degree_mapping_chunk(chunk):
     '''Chunk-based wrapper around compute_degree_mapping, to evaluate on chunks in parallel.'''
-    return [compute_degree_mapping(idx, name, nbrs, G, use_d, max_deg_bnd) 
-            for (idx, name, nbrs, G, use_d, max_deg_bnd) in chunk]
+    return [compute_degree_mapping(idx, name, nbrs, G, use_d, use_rel, max_deg_bnd) 
+            for (idx, name, nbrs, G, use_d, use_rel, max_deg_bnd) in chunk]
 
 
-def compute_degree_mapping(node_index, node_name, neighbours, G, use_distances, max_degree_bound):
+def compute_degree_mapping(node_index, node_name, neighbours, G, use_distances, use_relative_degrees, max_degree_bound):
     '''Degree mapping computation as a stateless function to call in parallel.'''
     G_n = G.induced_subgraph(neighbours)
-    deg = [node_deg if node_deg < max_degree_bound else max_degree_bound - 1 for node_deg in G_n.degree()]
+    sub_n = next(v for v in G_n.vs if v["name"] == node_name)
+    if use_relative_degrees or use_distances:
+        dist = [d[0] for d in G_n.shortest_paths_dijkstra(target=sub_n)]
+    if use_relative_degrees:
+        edge_degs = [get_relative_degrees(v, dist) for v in G_n.vs]
+        encoding = [tuple(min(d, max_degree_bound) for d in rel_degs) for rel_degs in edge_degs]
+    else:
+        encoding = [(d,) for d in G_n.degree()]
+    max_dist = 0
     if use_distances:
-        sub_n = next(v for v in G_n.vs if v['name'] == node_name)
-        deg_dist = zip((d[0] for d in G_n.shortest_paths_dijkstra(target=sub_n)), deg)
-        deg = [dist * max_degree_bound + deg for (dist, deg) in deg_dist]
-    return node_index, list(zip(*Counter(deg).most_common()))
+        max_dist = max(dist)
+        encoding = [[dist * max_degree_bound + d for d in deg] for deg, dist in zip(encoding, dist)]
+    max_encoding = max_degree_bound * (max_dist + 1)
+    encoding = [[feat_index + i * max_encoding for i, feat_index in enumerate(enc)] for enc in encoding]
+    return node_index, list(zip(*Counter([e for enc in encoding for e in enc]).most_common()))
 
 
 class StructuralMapper:
@@ -32,7 +58,8 @@ class StructuralMapper:
                  num_workers=cpu_count(),
                  cache_field='neigh_deg',
                  pack_as_arrays=False,
-                 device=None):
+                 device=None,
+                 use_relative_degrees=True):
         self.distance = distance
         self.use_distances = use_distances
         self.cache_field = cache_field
@@ -41,6 +68,7 @@ class StructuralMapper:
         self.source_G = G
         self.device = device
         self.pack_as_arrays = pack_as_arrays
+        self.use_relative_degrees = use_relative_degrees
 
     def pack_mapping_as_array(self, mapping):
         degree, count = mapping
@@ -48,7 +76,7 @@ class StructuralMapper:
                 np.asarray(count, dtype=np.int32))
 
     def num_elements(self):
-        return (self.distance + 1) * self.max_degree_bound
+        return (self.distance + 1) * self.max_degree_bound * (3 if self.use_relative_degrees else 1)
 
     def mapping_matrix(self, matrix_size):
         G = self.source_G
@@ -61,8 +89,9 @@ class StructuralMapper:
 
         matrix = (matrix - 0.5) / math.sqrt(matrix_size)
         for i in range(1, total_elements):
-            degree = (i % self.max_degree_bound)
-            distance = (i // self.max_degree_bound)
+            rel_index = i % ((self.distance + 1) * self.max_degree_bound)
+            degree = (rel_index % self.max_degree_bound)
+            distance = (rel_index // self.max_degree_bound)
             matrix[i] *= (math.exp(distance) / math.log(degree + 1)) if degree else 0.0
         matrix = (matrix - matrix.mean(axis=0)) / matrix.std(axis=0)
         return matrix
@@ -79,13 +108,14 @@ class StructuralMapper:
                                                     neighbours, 
                                                     G, 
                                                     self.use_distances, 
+                                                    self.use_relative_degrees,
                                                     self.max_degree_bound)
                 if self.pack_as_arrays:
                     mapping = self.pack_mapping_as_array(mapping)
                 deg_seq.append(mapping)
         else:
             pool = Pool(processes=self.num_workers)
-            as_tuples = [(n.index, n['name'], nbrs, G, self.use_distances, self.max_degree_bound) 
+            as_tuples = [(n.index, n['name'], nbrs, G, self.use_distances, self.use_relative_degrees, self.max_degree_bound) 
                          for (n, nbrs) in zip(node_seq, neighbours_seq)]
             chunks = [as_tuples[x::self.num_workers] for x in range(self.num_workers)]
             chunk_deg = pool.map(compute_degree_mapping_chunk, chunks)
